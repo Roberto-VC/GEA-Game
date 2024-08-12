394a111dd6982c4187cc9127029d90e77408996e
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2024-08-12 17:28:38 -0600

    Game

---
+++ Scene/DemoGame/Collision.h
@@ -0,0 +1,21 @@
+//
+// Created by rober on 1/08/2024.
+//
+
+#ifndef COLLISION_H
+#define COLLISION_H
+struct BrickComponent {
+
+};
+
+struct BallComponent {
+
+};
+
+struct PlayerComponent{
+};
+
+
+
+
+#endif //COLLISION_H
